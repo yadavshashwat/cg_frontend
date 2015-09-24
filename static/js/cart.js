a8b25957b04a4e91cd73fdf6ce18767237ece009
Vociferous <rajeev.dropamail@gmail.com>
2015-09-24 14:29:44 +0530

    cart

--- static/js/cart.js
+++ static/js/cart.js
@@ -6,5 +6,24 @@ var Global = {
         _this.setLayout();
         _this.eventHandlers();
         Commons.eventHandlers();
+        $('#car-select-box').trigger('change');
+    },
+    setLayout : function(){
+        var _this = this;
+    },
+    eventHandlers : function(){
+        var _this = this;
+        $('#car-select-box').on("change", function(){
+            var carVal = $(this).val();
+            console.log(carVal);
+            $('table.cart-table').hide();
+            $('table.cart-table[data-id="'+carVal+'"]').show();
+        });
     }
+
 }
+
+$(document).ready(function(){
+    Global.init();
+});
+
