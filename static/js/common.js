fc5208e3e4ccc0797b63c961af84d9857776bf40
yadavshashwat <y.shashwat@gmail.com>
2015-10-12 22:47:08 +0530

    conflict resolve

--- static/js/common.js
+++ static/js/common.js
@@ -347,25 +347,40 @@ var Templates = {
             "tag":"div","class":"service-list-item minimized", "data-id":"${id}", "children":[
                 {"tag":"div", "class":"top-row", "children":[
                     {"tag":"div", "class":"wrapper detail-wrapper", "children":[
-                        {"tag":"div", "id":"cancel-booking",  "tran_id":"${tran_id}", "class":"vendor-div", "html":function(){return "<div> Cancel Booking </div>";}}
+                        {"tag":"div", "id":"cancel-booking",  "tran_id":"${tran_id}", "class":"vendor-div none-i", "html":function(){return "<div> Cancel Booking </div>";}}
                     ]},
 
                     {"tag":"div", "class":"wrapper header-wrapper", "children":[
                         {"tag":"div", "class":"booking id booking_id", "html":function(){return '#'+(this.booking_id);}},
                         {"tag":"div", "class":"booking car booking_car", "html":function(){return (this.cust_carname);}},
                         {"tag":"div", "class":"booking date booking_date", "html":function(){return (this.date_booking) + ' ('+this.time_booking+')';}},
-                        {"tag":"div", "class":"booking list booking_list", "html":function(){
-                            var s = '<ul class="service-components">'
-                            $.each(this.service_items,function(i,elem){
-                                if(elem.service == 'servicing'){
-                                s += '<li class="indiv_booking">'+elem.served_data.dealer_cat+'  <i class="fa fa-caret-right"></i>  '+ elem.served_data.odometer+'km (Regular Sevicing) </li>'
-                                }else{
-                                s += '<li class="indiv_booking">'+elem.served_data.vendor +'  <i class="fa fa-caret-right"></i>  '+ elem.served_data.service+' ( '+elem.served_data.category+' ) </li>'
-                                }
-                            })
-                            s += '</ul>'
-                            return s
-                        }
+                        {"tag":"div", "class":"booking list booking_list",
+                        //    "html":function(){
+                        //    var s = '<ul class="service-components">'
+                        //    $.each(this.service_items,function(i,elem){
+                        //        if(elem.service == 'servicing'){
+                        //        s += '<li class="indiv_booking" style="height: 90px;">'+elem.served_data.dealer_cat+'  <i class="fa fa-caret-right"></i>  '+ elem.served_data.odometer+'km (Regular Sevicing) <div id="cancel-booking" tran_id="561ac69396f69553e74fe545" class="vendor-div" style="padding: 0px; float: right;"><div> Cancel Booking </div></div></li>'
+                        //        }else{
+                        //        s += '<li class="indiv_booking" style="height: 90px;">'+elem.served_data.vendor +'  <i class="fa fa-caret-right"></i>  '+ elem.served_data.service+' ( '+elem.served_data.category+' ) <div id="cancel-booking" tran_id="561ac69396f69553e74fe545" class="vendor-div" style="padding: 0px; float: right;"><div> Cancel Booking </div></div></li>'
+                        //        }
+                        //    })
+                        //    s += '</ul>'
+                        //    return s
+                        //},
+                        "children": [
+                            {"tag":"ul", "class":"service-components", "html":function(){
+                                var _booking = this;
+                                var s = ''
+                                $.each(this.service_items,function(i,elem){
+                                    if(elem.service == 'servicing'){
+                                    s += '<li class="indiv_booking" style="height: 90px;">'+elem.served_data.dealer_cat+'  <i class="fa fa-caret-right"></i>  '+ elem.served_data.odometer+'km (Regular Sevicing) <div id="cancel-booking" tran_id="'+_booking.tran_id+'" item_id="'+elem.served_data.ts+'" class="vendor-div" style="padding: 0px; float: right;"><div> Cancel Booking </div></div></li>'
+                                    }else{
+                                    s += '<li class="indiv_booking" style="height: 90px;">'+elem.served_data.vendor +'  <i class="fa fa-caret-right"></i>  '+ elem.served_data.service+' ( '+elem.served_data.category+' ) <div id="cancel-booking" tran_id="'+_booking.tran_id+'" item_id="'+elem.served_data.ts+'" class="vendor-div" style="padding: 0px; float: right;"><div> Cancel Booking </div></div></li>'
+                                    }
+                                })
+                                return s;
+                            }}
+                        ]
                         },    
                         
                         ]},
