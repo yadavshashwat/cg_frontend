d024d242ec98c1dea329129a393dc60fecbfb954
yadavshashwat <y.shashwat@gmail.com>
2015-10-15 03:13:31 +0530

    bookings

--- static/js/common.js
+++ static/js/common.js
@@ -418,12 +418,12 @@ var Templates = {
                 ]},
         
             ]
-        }]},
+        }],
         booking:[{
                 "tag":"div","class":"service-list-item minimized", "data-id":"${id}", "children":[
                     {"tag":"div", "class":"top-row", "children":[
                         {"tag":"div", "class":"wrapper detail-wrapper", "children":[
-                            {"tag":"div", "id":"cancel-booking",  "tran_id":"${tran_id}", "class":"vendor-div", "html":function(){return "<div> Cancel Booking </div>";}}
+                            {"tag":"div", "id":"cancel-booking",  "tran_id":"${tran_id}", "class":"vendor-div", "html":function(){return '<div> Cancel Booking </div>';}}
                         ]},
 
                         {"tag":"div", "class":"wrapper header-wrapper", "children":[
@@ -431,7 +431,7 @@ var Templates = {
                             {"tag":"div", "class":"booking car booking_car", "html":function(){return (this.cust_carname);}},
                             {"tag":"div", "class":"booking date booking_date", "html":function(){return (this.date_booking) + ' ('+this.time_booking+')';}},
                             {"tag":"div", "class":"booking list booking_list", "html":function(){
-                                var s = '<ul class="service-components">'
+                                var s = '<ul cslass="service-components">'
                                 $.each(this.service_items,function(i,elem){
                                     if(elem.service == 'servicing'){
                                     s += '<li class="indiv_booking">'+elem.served_data.dealer_cat+'  <i class="fa fa-caret-right"></i>  '+ elem.served_data.odometer+'km (Regular Sevicing) </li>'
@@ -496,7 +496,7 @@ var Templates = {
                 },
 
             ]
-        }]
+        }]}
 
 };
 
