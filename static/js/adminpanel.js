2ee8129da316c1b8f71299ad5e13a3aae4fb491d
yadavshashwat <y.shashwat@gmail.com>
2015-11-16 02:04:42 +0530

    adming panel

--- static/js/adminpanel.js
+++ static/js/adminpanel.js
@@ -5,6 +5,7 @@ var Global = {
 //    $("[data-role=panel]").panel().enhanceWithin();  
         _this.events();
         //$('#submit-button').button('disable');
+
     },
     events:function(){
         var _this = this;
@@ -16,11 +17,190 @@ var Global = {
 //          console.log( data.state.url )
 //          console.log( data.state.hash )
 //          $("#test").text("new text");
-          Commons.ajaxData('fetch_all_booking', {r_id:"dmFydW5ndWxhdGlsaWtlc2dhbG91dGlrZWJhYg=="},"get",_this, _this.loadBookings)
+          Commons.ajaxData('fetch_all_booking', {},"get",_this, _this.loadBookings)
 //          Commons.ajaxData('fetch_all_cars', {},"get",_this, _this.loadBookings)
             }
         );
-        
+
+        $('#send-button1').on('click', function(e){
+             var number_cust = document.getElementById('number_cust');
+             number_cust_content =  number_cust.textContent;
+
+             var src = document.getElementById("service_centre").value;
+             $('.sms-popup .vendor-details').text(src);
+
+             var number_driver = document.getElementById('driver_details');
+             number_driver_content =  String(number_driver.textContent);
+             var n = parseInt(number_driver_content.search("Mob")) + 5;
+             var driver_number = number_driver_content.substring(n ,n+10);
+
+             var number_advisor = document.getElementById('advisor_details');
+             number_advisor_content =  String(number_advisor.textContent);
+             var k = parseInt(number_advisor_content.search("Mob")) + 5;
+             var advisor_number = number_advisor_content.substring(k ,k+10);
+
+             var smstext = document.getElementById('sms-content0');
+             smstext_content = smstext.textContent.trim();
+
+             var url = "http://sms.hspsms.com:8090/sendSMS?username=clickgarage&message="+ smstext_content + "&sendername=CLKGRG&smstype=TRANS&numbers=" + driver_number + "&apikey=ab33f626-fba5-4bff-9a2b-68a7e9eed43c"
+             //window.location(url);
+             var xhr = new XMLHttpRequest();
+             xhr.open("GET", url, false);
+             xhr.send();
+            }
+        );
+
+        $('#send-button2').on('click', function(e){
+             var number_cust = document.getElementById('number_cust');
+             number_cust_content =  number_cust.textContent.trim();
+
+             var number_driver = document.getElementById('driver_details');
+             number_driver_content =  String(number_driver.textContent);
+             var n = parseInt(number_driver_content.search("Mob")) + 5;
+             var driver_number = number_driver_content.substring(n ,n+10);
+
+             var number_advisor = document.getElementById('advisor_details');
+             number_advisor_content =  String(number_advisor.textContent);
+             var k = parseInt(number_advisor_content.search("Mob")) + 5;
+             var advisor_number = number_advisor_content.substring(k ,k+10);
+
+             var smstext = document.getElementById('sms-content1');
+             smstext_content = smstext.textContent.trim();
+
+             var url = "http://sms.hspsms.com:8090/sendSMS?username=clickgarage&message="+ smstext_content + "&sendername=CLKGRG&smstype=TRANS&numbers=" + number_cust_content + "&apikey=ab33f626-fba5-4bff-9a2b-68a7e9eed43c"
+             var xhr = new XMLHttpRequest();
+             xhr.open("GET", url, false);
+             xhr.send();
+            }
+        );
+
+        $('#send-button3').on('click', function(e){
+             var number_cust = document.getElementById('number_cust');
+             number_cust_content =  number_cust.textContent.trim();
+
+             var number_driver = document.getElementById('driver_details');
+             number_driver_content =  String(number_driver.textContent);
+             var n = parseInt(number_driver_content.search("Mob")) + 5;
+             var driver_number = number_driver_content.substring(n ,n+10);
+
+             var number_advisor = document.getElementById('advisor_details');
+             number_advisor_content =  String(number_advisor.textContent);
+             var k = parseInt(number_advisor_content.search("Mob")) + 5;
+             var advisor_number = number_advisor_content.substring(k ,k+10);
+
+             var smstext = document.getElementById('sms-content2');
+             smstext_content = smstext.textContent.trim();
+
+             var url = "http://sms.hspsms.com:8090/sendSMS?username=clickgarage&message="+ smstext_content + "&sendername=CLKGRG&smstype=TRANS&numbers=" + number_cust_content + "&apikey=ab33f626-fba5-4bff-9a2b-68a7e9eed43c"
+             var xhr = new XMLHttpRequest();
+             xhr.open("GET", url, false);
+             xhr.send();
+            }
+        );
+
+        $('#send-button4').on('click', function(e){
+             var number_cust = document.getElementById('number_cust');
+             number_cust_content =  number_cust.textContent.trim();
+
+
+             var number_driver = document.getElementById('driver_details');
+             number_driver_content =  String(number_driver.textContent);
+             var n = parseInt(number_driver_content.search("Mob")) + 5;
+             var driver_number = number_driver_content.substring(n ,n+10);
+
+             var number_advisor = document.getElementById('advisor_details');
+             number_advisor_content =  String(number_advisor.textContent);
+             var k = parseInt(number_advisor_content.search("Mob")) + 5;
+             var advisor_number = number_advisor_content.substring(k ,k+10);
+
+             var now = new Date()
+             var time = [ now.getHours(), now.getMinutes() ];
+
+            // Determine AM or PM suffix based on the hour
+              var suffix = ( time[0] < 12 ) ? "AM" : "PM";
+
+            // Convert hour from military time
+              time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
+
+            // If hour is 0, set it to 12
+              time[0] = time[0] || 12;
+
+            // If seconds and minutes are less than 10, add a zero
+              for ( var i = 1; i < 3; i++ ) {
+                if ( time[i] < 10 ) {
+                  time[i] = "0" + time[i];
+                }
+              }
+             var time_now = time.join(":") + " " + suffix;
+
+             $('.sms-popup .current-time').text(time_now);
+
+             var smstext = document.getElementById('sms-content3');
+             smstext_content = smstext.textContent.trim();
+
+
+
+             var url = "http://sms.hspsms.com:8090/sendSMS?username=clickgarage&message="+ smstext_content + "&sendername=CLKGRG&smstype=TRANS&numbers=" + number_cust_content + "&apikey=ab33f626-fba5-4bff-9a2b-68a7e9eed43c"
+             var xhr = new XMLHttpRequest();
+             xhr.open("GET", url, false);
+             xhr.send();
+            }
+        );
+        $('#send-button5').on('click', function(e){
+             var number_cust = document.getElementById('number_cust');
+             number_cust_content =  number_cust.textContent.trim();
+
+             var number_driver = document.getElementById('driver_details');
+             number_driver_content =  String(number_driver.textContent);
+             var n = parseInt(number_driver_content.search("Mob")) + 5;
+             var driver_number = number_driver_content.substring(n ,n+10);
+
+             var number_advisor = document.getElementById('advisor_details');
+             number_advisor_content =  String(number_advisor.textContent);
+             var k = parseInt(number_advisor_content.search("Mob")) + 5;
+             var advisor_number = number_advisor_content.substring(k ,k+10);
+
+             var amount = document.getElementById("due_amount").value;
+             $('.sms-popup .due-amount').text(amount);
+
+             var smstext = document.getElementById('sms-content4');
+             smstext_content = smstext.textContent.trim();
+
+             var url = "http://sms.hspsms.com:8090/sendSMS?username=clickgarage&message="+ smstext_content + "&sendername=CLKGRG&smstype=TRANS&numbers=" + number_cust_content + "&apikey=ab33f626-fba5-4bff-9a2b-68a7e9eed43c"
+             var xhr = new XMLHttpRequest();
+             xhr.open("GET", url, false);
+             xhr.send();
+             //   alert(amount);
+            }
+        );
+        $('#send-button6').on('click', function(e){
+             var number_cust = document.getElementById('number_cust');
+             number_cust_content =  number_cust.textContent.trim();
+
+             var number_driver = document.getElementById('driver_details');
+             number_driver_content =  String(number_driver.textContent);
+             var n = parseInt(number_driver_content.search("Mob")) + 5;
+             var driver_number = number_driver_content.substring(n ,n+10);
+
+             var number_advisor = document.getElementById('advisor_details');
+             number_advisor_content =  String(number_advisor.textContent);
+             var k = parseInt(number_advisor_content.search("Mob")) + 5;
+             var advisor_number = number_advisor_content.substring(k ,k+10);
+
+             //var amount = document.getElementById("due_amount").value;
+             //$('.sms-popup .due-amount').text(amount);
+
+             var smstext = document.getElementById('sms-content5');
+             smstext_content = smstext.textContent.trim();
+
+             var url = "http://sms.hspsms.com:8090/sendSMS?username=clickgarage&message="+ smstext_content + "&sendername=CLKGRG&smstype=TRANS&numbers=" + number_cust_content + "&apikey=ab33f626-fba5-4bff-9a2b-68a7e9eed43c"
+             var xhr = new XMLHttpRequest();
+             xhr.open("GET", url, false);
+             xhr.send();
+             //   alert(amount);
+            }
+        );
+
 //    $('.action-button').on('click', function(e){
     $('.centered-content').on('click', '#sms-button', function(e){
         console.log('testing');
@@ -44,14 +224,14 @@ var Global = {
         $('.sms-popup .cust-address').text(obj.cust_address);
         $('.sms-popup .vehicle').text(obj.brand + ' ' + obj.make);
         $('.sms-popup .pickup-time').text(obj.pickup_time);
+        //$('.sms-popup .current-time').text(obj.pickup_time);
         $('.sms-popup').show()
             }
         );
-        
-        
+
     $('#select-driver').on('change', function(e){
         var driver_det = $('#select-driver option:selected').text();
-        $('.sms-popup .driver-details').text(driver_det);        
+        $('.sms-popup .driver-details').text(driver_det);
             }
         );
         
@@ -66,7 +246,7 @@ var Global = {
 
     loadBookings : function(data){
         console.log(data)
-        var container = $('.booking');
+        var container = $('.section-listings .booking');
         container.html('');
         var html = '';
         $.each(data, function(idx, val){
@@ -75,17 +255,29 @@ var Global = {
             html += '<div class="booking-entry-cat" id="id-div">'
             html += '<p>' + String(val.booking_id) + '</p>';
             html += '</div>';
-//            service-div starts here
+////            service-div starts here
             html += '<div class="booking-entry-cat" id="service-div">';
+
+            try{
             html += '<p id="service-type">Service: ' + String(val.service_items[0].served_data.type_service) + '</p>';
             html += '<p id="vendor-type">Vendor: ' + String(val.service_items[0].served_data.dealer_cat) + '</p>';
+                }
+            catch(err){
+                html += '<p id="service-type">Service: ' + String(val.service_items[0].service) + '</p>';
+                //html += '<p id="vendor-type">Vendor: ' + String(val.service_items[0].served_data.dealer_cat) + '</p>';
+            }
             html += '<p id="pickup-time">Pickup-time: ' + String(val.time_booking) + '</p>';
             html += '</div>';
-//            car-div starts
+////            car-div starts
             html += '<div class="booking-entry-cat" id="car-div">';
-            html += '<p id="brand">Brand: ' + String(val.service_items[0].served_data.brand) + '</p>';
-            html += '<p id="make">Make: ' + String(val.service_items[0].served_data.car).replace(String(val.service_items[0].served_data.brand),"") + '</p>';
-            html += '<p id="regn">Regn#: ' + String(val.cust_carnumber) + '</p>';
+            try {
+                html += '<p id="brand">Brand: ' + String(val.service_items[0].served_data.brand) + '</p>';
+                html += '<p id="make">Make: ' + String(val.service_items[0].served_data.car).replace(String(val.service_items[0].served_data.brand), "") + '</p>';
+                html += '<p id="regn">Regn#: ' + String(val.cust_carnumber) + '</p>';
+            }
+            catch(err){
+
+            }
             html += '</div>';
 //            cust-div starts
             html += '<div class="booking-entry-cat" id="cust-div">';
