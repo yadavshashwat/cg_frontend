918347b9a278f7e77bf15808f236ad99954d0fca
Vociferous <rajeev.dropamail@gmail.com>
2016-04-10 22:10:11 +0530

    ad files

---
+++ static/js/ad/creative.js
@@ -0,0 +1,57 @@
+/*!
+ * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Initialize WOW.js Scrolling Animations
+    new WOW().init();
+    $('.clean-inp-wrapper').off().on('focus', 'input.clean-inp-tbox', function(e){
+        $(this).closest('.clean-inp-wrapper').addClass('input-focused');
+    });
+    $('.clean-inp-wrapper').on('focusout', 'input.clean-inp-tbox', function(e){
+        if(!$(this).val() && !$(this).hasClass('perpetual')){
+            $(this).closest('.clean-inp-wrapper').removeClass('input-focused');
+        }
+    });
+
+
+})(jQuery); // End of use strict
